77d66f771d04e8fe35afa31b35a16a3f0e9fd2de
ipdbco <41750787+ipdbco@users.noreply.github.com>
2018-08-07 09:08:57 -0400

    replaced example details with up to date info

--- setup.py
+++ setup.py
@@ -2,27 +2,29 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+with open("LICENSE", "r") as fh:
+    license = fh.read()
 
 requirements = [
     'requests',
-    'sanic',
     'maxminddb'
 ]
 
 setuptools.setup(
     name="ip_db",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="ipdbco_tuan",
+    author_email="41750787+ipdbco@users.noreply.github.com",
+    description="A small Python 3 module to manage IP address databases used for resolving geolocation and getting threat information aboutIP address",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ipdbco/ip_db",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3.0",
         "Operating System :: OS Independent",
     ),
     install_requires=requirements,
+    license=license,
 )
